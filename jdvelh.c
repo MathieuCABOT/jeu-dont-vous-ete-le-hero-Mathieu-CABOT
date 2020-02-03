38af78dfc7990c8a36fc9b02d7b255117f96af1e
MathieuCABOT <cab.mathieu.pro@gmail.com>
2020-02-03 14:31:44 +0100

    choix par numero

---
+++ jdvelh.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+    int choix = 0;
+
+
+
+        printf("\n");
+        printf("\n");
+        printf("[1] pour la destination 1\n");
+        printf("[2] pour la destination 2\n");
+      
+    scanf("%d", &choix);
+
+      if (choix == 1){
+
+      		printf("destination 1");
+      		printf("\n");
+      }
+
+
+
+      if (choix == 2){
+
+      	  	printf("destination 1");
+      		printf("\n");
+
+      }
